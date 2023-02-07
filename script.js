01efbeb7dcf5820b5711ab7e4f0d73022520b2ba
Ankur Semle <semleankur@gmail.com>
2023-02-07 16:40:18 +0530

    logic and functionality done

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const billAmount = document.getElementById("#bill-amount");
-const cashGiven = document.getElementById("#cash-given");
-const checkButton = document.getElementById("#check-button");
-const message = document.getElementById("#error-message");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click" , function validateBillAndCashAmount() {
     hideMessage();
     if(billAmount.value>0){
-        if(cashGiven.value >= billAmount.value){
+        if(cashGiven.value >= billAmount.value){ // 2022 - 12 = 2010
             const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+            calculateChange(amountToBeReturned); // 2010
         }
         else{
             showMessage("Do you wanna wash plates?");
@@ -19,8 +22,18 @@ checkButton.addEventListener("click" , function validateBillAndCashAmount() {
     }
 });
 
-function calculateChange(){
+function calculateChange(amountToBeReturned){
+   
+    for(var i =0; i<availableNotes.length;i++){
+         // no of notes needed for the denomination
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]); // 2010/2000 = 1 || 10/500 = 0
+        // amount left after calculating the number of notes needed
+        amountToBeReturned %= availableNotes[i];
+        // 2010 % 2000 = 10 || 10 % 500 = 10
 
+        //updating the no of notes in the table for the current amount
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
 
 function hideMessage(){
